ae9f1025a66e60bdca4b5e8dfe173128306224aa
Kingsley Chimezie <kingsleychimezie92@gmail.com>
2021-02-06 03:41:10 +0000

    Added loader function and starting work on Firebase logic

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@
  */
 
 
-let dbData = {
-    lastAnswer: '',
-    lastAnswerDate: '',
-    lastAnswerIP: '',
-    submissionsArr: []
-};
-
-// TODO: PERSIST DATA
-
-// Get elements
+/* VARIABLES DECLARATION
+************************************************************************************/
+ // Get elements
 let header = document.getElementById('header');
 let loader = document.getElementById('loader');
 let resultDiv = document.getElementById('result');
@@ -24,66 +17,89 @@ let btnNo = document.getElementById('btnNo');
 let btnChangeAnswer = document.getElementById('btnChangeAnswer');
 let lastAnswerDateText = document.getElementById('lastAnswerDate');
 
-
-const dataProcessor = (doc) => {
-
-    // set header title
-    header.innerHTML = `${data.valentinesFirstName} ${data.valentinesLastName}, <br> ${data.headerMessage}`;
-
-    // No answer yet
-    if (dbData.lastAnswer === '') {
-        loader.style.display = 'block';
-        setTimeout(() => { loader.style.display = 'none'; }, 1500);
-        btnYes.style.display = 'inline';
-        btnNo.style.display = 'inline';
-        resultDiv.style.display = 'none';
-        btnChangeAnswer.style.display = 'none';
-        lastAnswerDateText.style.display = 'none';
-    }
-    // Answer is yes
-    else if (dbData.lastAnswer === 'yes') {
-        loader.style.display = 'block';
-        setTimeout(() => { loader.style.display = 'none'; }, 1500);
-        resultHeader.innerHTML = data.resultHeaderYes;
-        lastAnswerDateText.innerHTML = `${data.lastAnswerHeading} ${dbData.lastAnswerDate}`;
-        resultImg.src = data.yesImgLink;
-        resultDiv.style.display = 'block';
-        btnChangeAnswer.style.display = 'inline';
-        lastAnswerDateText.style.display = 'block';
-        btnYes.style.display = 'none';
-        btnNo.style.display = 'none';
-    }
-    // Answer is no
-    else if (dbData.lastAnswer === 'no') {
-        loader.style.display = 'block';
-        setTimeout(() => { loader.style.display = 'none'; }, 1500);
-        resultHeader.innerHTML = data.resultHeaderNo;
-        lastAnswerDateText.innerHTML = `${data.lastAnswerHeading} ${dbData.lastAnswerDate}`;
-        resultImg.src = data.noImgLink;
-        resultDiv.style.display = 'block';
-        btnChangeAnswer.style.display = 'inline';
-        lastAnswerDateText.style.display = 'block';
-        btnYes.style.display = 'none';
-        btnNo.style.display = 'none';
-    }
+// Object used to retrieve and send data to DB
+let dbData = {
+    lastAnswer: '',
+    lastAnswerDate: '',
+    lastAnswerIP: '',
+    submissionsArr: []
 };
-dataProcessor();
+
+let cloudFirestoreIdExists = false;
+/************************************************************************************/
+
+
+
+/* FIREBASE
+************************************************************************************/
+
+// INITIALISE FIREBASE
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const db = firebase.firestore();
+
+
+/* GET FIREBASE DATA
+-------------------------------- */
+db.collection('valentines').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        docData = doc.data();
+        // check if defined ID exists in DB
+        if (doc.id === data.cloudFirestoreId) {
+            cloudFirestoreIdExists = true;
+
+            console.log(Object.keys(docData).length === 0 && docData.constructor === Object);
+        }
+    });
+});
+const setLocalObjToFirebaseData = (doc) => {
+    console.log("setLocalObjToFirebaseData");
+    // setting retrived Firbase data
+    data.valentinesFirstName = doc.data().valentinesFirstName;
+    data.valentinesLastName = doc.data().valentinesLastName;
+    data.lastAnswerHeading = doc.data().lastAnswerHeading;
+    data.headerMessage = doc.data().headerMessage;
+    data.resultHeaderYes = doc.data().resultHeaderYes;
+    data.resultHeaderNo = doc.data().resultHeaderNo;
+    data.yesImgLink = doc.data().yesImgLink;
+    data.noImgLink = doc.data().noImgLink;
+};
+/************************************************************************************/
+
+
+/* LOADER
+-------------------------------- */
+const load = () => {
+    // scroll to top
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+    // hide UI content
+    header.style.display = 'none';
+    loader.style.display = 'block';
+
+    setTimeout(() => { 
+        loader.style.display = 'none'; 
+        header.style.display = 'inline';
+    }, 2000);
+};
+
 
 /* BUTTONS CLICKED
 -------------------------------- */
 const yesClicked = () => {
     setData('yes');
-    dataProcessor();
+    setUI();
 };
 
 const noClicked = () => {
     setData('no');
-    dataProcessor();
+    setUI();
 };
 
 const changeAnswerClicked = () => {
     setData('');
-    dataProcessor();
+    setUI();
 };
 
 
@@ -118,11 +134,54 @@ const setData = (answer) => {
         
         // push to submission tracking array / DB
         dbData.submissionsArr.push({
-            lastAnswer: dbData.lastAnswer,
-            lastAnswerDate: dbData.lastAnswerDate,
-            lastAnswerIP: dbData.lastAnswerIP
+            answer: dbData.lastAnswer,
+            answerDate: dbData.lastAnswerDate,
+            submissionIP: dbData.lastAnswerIP
         });
-        console.log(dbData.submissionsArr);
+        console.log(dbData);
     }
 };
 
+
+/* SET UI 
+-------------------------------- */
+const setUI = (doc) => {
+
+    // set header title
+    header.innerHTML = `${data.valentinesFirstName} ${data.valentinesLastName}<br>${data.headerMessage}`;
+
+    // No answer yet
+    if (dbData.lastAnswer === '') {
+        load();
+        btnYes.style.display = 'inline';
+        btnNo.style.display = 'inline';
+        resultDiv.style.display = 'none';
+        btnChangeAnswer.style.display = 'none';
+        lastAnswerDateText.style.display = 'none';
+    }
+    // Answer is yes
+    else if (dbData.lastAnswer === 'yes') {
+        load();
+        resultHeader.innerHTML = data.resultHeaderYes;
+        lastAnswerDateText.innerHTML = `${data.lastAnswerHeading} ${dbData.lastAnswerDate}`;
+        resultImg.src = data.yesImgLink;
+        resultDiv.style.display = 'block';
+        btnChangeAnswer.style.display = 'inline';
+        lastAnswerDateText.style.display = 'block';
+        btnYes.style.display = 'none';
+        btnNo.style.display = 'none';
+    }
+    // Answer is no
+    else if (dbData.lastAnswer === 'no') {
+        load();
+        resultHeader.innerHTML = data.resultHeaderNo;
+        lastAnswerDateText.innerHTML = `${data.lastAnswerHeading} ${dbData.lastAnswerDate}`;
+        resultImg.src = data.noImgLink;
+        resultDiv.style.display = 'block';
+        btnChangeAnswer.style.display = 'inline';
+        lastAnswerDateText.style.display = 'block';
+        btnYes.style.display = 'none';
+        btnNo.style.display = 'none';
+    }
+};
+setUI();
